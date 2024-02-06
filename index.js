bfd2a5b494c3958bac0024f6145f074391e0867f
Kane2881 <kanethepro2881@gmail.com>
2024-02-06 15:16:41 +0630

    Commit initial files

---
+++ index.js
@@ -0,0 +1,33 @@
+let homeScoreEL = document.getElementById("home-score");
+let guestScoreEL = document.getElementById("guest-score");
+
+let homeScore = 0;
+let guestScore = 0;
+
+function homePlusOne() {
+  homeScore += 1;
+  homeScoreEL.textContent = homeScore;
+}
+
+function homePlusTwo() {
+  homeScore += 2;
+  homeScoreEL.textContent = homeScore;
+}
+
+function homePlusThree() {
+  homeScore += 3;
+  homeScoreEL.textContent = homeScore;
+}
+
+function guestPlusOne() {
+  guestScore += 1;
+  guestScoreEL.textContent = guestScore;
+}
+function guestPlusTwo() {
+  guestScore += 2;
+  guestScoreEL.textContent = guestScore;
+}
+function guestPlusThree() {
+  guestScore += 3;
+  guestScoreEL.textContent = guestScore;
+}
